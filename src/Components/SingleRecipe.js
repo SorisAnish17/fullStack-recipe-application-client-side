8a6a63f351f7c4be4f99a37df8cd3cfcddf820b8
Soris Anish <125728458+SorisAnish17@users.noreply.github.com>
2023-09-18 20:09:15 +0530

    Update SingleRecipe.js

--- src/Components/SingleRecipe.js
+++ src/Components/SingleRecipe.js
@@ -29,7 +29,7 @@ const SingleRecipe = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/api/v1/user/${Id}`)
+      .get(`https://sore-cyan-hedgehog-slip.cyclic.cloud/api/v1/user/${Id}`)
       .then((response) => {
         if (response.data.data) {
           setUserDetail(response.data.data);
